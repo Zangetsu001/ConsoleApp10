ffea66ac8724f3a572c42bf775f4b3d3f254056c
qdedm <qdedm@DESKTOP-LFEL1F2>
2024-02-13 09:47:23 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 1;
+            //while (x <= 10)
+            //{
+            //    Console.WriteLine(x);
+            //    //x=x+1;
+            //    //x+=1;
+            //    x++;
+            //}
+            //Console.ReadLine();
+
+
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int x = 1;
+            //while (x <= n)
+            //{
+            //    Console.WriteLine(x);
+            //    //x=x+1;
+            //    //x+=1;
+            //    x++;
+            //}
+            //Console.ReadLine();
+
+
+
+            //int x = 1;
+            //while (x <= 10)
+            //{
+            //    Console.WriteLine("Hello");
+            //    //x=x+1;
+            //    //x+=1;
+            //    x++;
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
